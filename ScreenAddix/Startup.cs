6b957864a3126d49caa369a80730e7095ede00ed
Scott Doran <Scott Doran>
2018-01-09 18:46:17 +0000

    Add project files.

---
+++ ScreenAddix/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ScreenAddix.Startup))]
+namespace ScreenAddix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
